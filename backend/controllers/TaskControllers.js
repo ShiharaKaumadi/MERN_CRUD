5b0af883105ecacf6aa782ec28b77cc15705a741
ShiharaKaumadi <eshani9544@gmail.com>
2024-08-02 17:47:01 +0530

    Save, Update, Delete Task

--- backend/controllers/TaskControllers.js
+++ backend/controllers/TaskControllers.js
@@ -7,12 +7,42 @@ module.exports.getTasks =async (req,res)=>{ //export function 'getTasks' as a pa
 }
 
 //saveTasks function : saving new task in database and send HTTTP response to client (responsible for saving new tasks in database)
-module.exports.saveTasks = (req,res)=>{ //make 'saveTasks' function accessible from all files in the project
+module.exports.saveTask = (req,res)=>{ //make 'saveTasks' function accessible from all files in the project
     const {task} = req.body;// use destructuring to extract 'tasks' from request body
+
     TaskModel.create({task}) // creates  a new document in mongoDB collection associate with the 'TaskModel' model
         .then((data) => {
             console.log("Saved Successfully!");
             res.status(201).send(data);
+        }).catch((err)=>{
+        console.log(err);
+        res.send({error:err,msg:"Something went wrong.."})
+    })
+};
+
+//updateTask function: handles updating of task details in database by uniquely identifying task by its id
+module.exports.updateTask = (req,res)=>{
+    const {id} = req.params;//extracting id from URL
+    const {task} = req.body;//extracting task from url to uniquely identify the task to be updated
+
+    TaskModel.findByIdAndUpdate(id,{task}) //find the task by id and update with new task details
+        .then(() => {
+            console.log("Updates Successfully!");
+            res.send("Updated");
+        }).catch((err)=>{
+        console.log(err);
+        res.send({error:err,msg:"Something went wrong.."})
+    })
+};
+
+//deleteTask function: handles deletion of task in database by uniquely identifying task by its id
+module.exports.deleteTask = (req,res)=>{
+    const {id} = req.params;//extracting id from URL to uniquely identify the task to be deleted
+
+    TaskModel.findByIdAndDelete(id,{task})
+        .then((data) => {
+            console.log("Deleted Successfully!");
+            res.send("Deleted");
         }).catch((err)=>{
         console.log(err);
         res.send({error:err,msg:"Something went wrong.."})
--- backend/routes/TaskRoute.js
+++ backend/routes/TaskRoute.js
@@ -1,6 +1,6 @@
 //1--->basic express router for handling HTTP GET request at '/get' endpoint
 
-const {getTasks} = require("../controllers/TaskControllers"); //import getTasks from TaskControllers
+const {getTasks, saveTask, updateTask, deleteTask} = require("../controllers/TaskControllers"); //import getTasks from TaskControllers
 
 //extract Router from Express
 const {Router} = require("express");//import Router class from Express Library
@@ -9,6 +9,12 @@ const router = Router();//creates an instance of Router class
 
 router.get("/get",getTasks);// defines route handler for HTTP GET request to the '/get' path.
 //When a GET request is made to this endpoint the 'getTasks()' function will be called to handle the request
+
+router.post("/save",saveTask);// defines route handler for HTTP POST request to the '/save' path.
+
+router.put("/update/:id",updateTask);// defines route handler for HTTP PUT request to the '/update/:id' path.
+
+router.delete("/delete/:id",deleteTask);// defines route handler for HTTP DELETE request to the '/delete/:id' path.
 
 module.exports = router// export router object
 // so other modules in the application can import and use the defined routes and middleware
