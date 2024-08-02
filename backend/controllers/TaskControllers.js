b5f619bc2d05085fe37b8ad11de03a74daf694e2
ShiharaKaumadi <eshani9544@gmail.com>
2024-08-02 16:51:10 +0530

    Retrieveing All tasks documents from database and send response to client

--- backend/controllers/TaskControllers.js
+++ backend/controllers/TaskControllers.js
@@ -6,3 +6,16 @@ module.exports.getTasks =async (req,res)=>{ //export function 'getTasks' as a pa
     res.send(tasks);// send th retrieved tasks as response to the client (send tasks array as HTTP response body in JSON format)
 }
 
+//saveTasks function : saving new task in database and send HTTTP response to client (responsible for saving new tasks in database)
+module.exports.saveTasks = (req,res)=>{ //make 'saveTasks' function accessible from all files in the project
+    const {task} = req.body;// use destructuring to extract 'tasks' from request body
+    TaskModel.create({task}) // creates  a new document in mongoDB collection associate with the 'TaskModel' model
+        .then((data) => {
+            console.log("Saved Successfully!");
+            res.status(201).send(data);
+        }).catch((err)=>{
+        console.log(err);
+        res.send({error:err,msg:"Something went wrong.."})
+    })
+};
+
